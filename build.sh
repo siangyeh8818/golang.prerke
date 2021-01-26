759bc2c9ebb512cca0caa8b09828dbc7518232dc
siangyeh8818 <siangyeh8818@gmail.com>
2021-01-26 14:33:26 +0800

    fix: fix binary name

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #mac 版的binary
-go build -o build/mac/prerke cmd/main.go
+go build -o build/mac/prerke_darwin-amd64
+ cmd/main.go
 
 #Linux版的binary
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build/linux/prerke cmd/main.go 
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build/linux/prerke_linux-amd64 cmd/main.go 
