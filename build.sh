faacf04c4888884d3a93a5e410db69ff769cf7ee
siangyeh8818 <siangyeh8818@gmail.com>
2021-01-26 14:34:35 +0800

    fix: build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 #mac 版的binary
-go build -o build/mac/prerke_darwin-amd64
- cmd/main.go
+go build -o build/mac/prerke_darwin-amd64 cmd/main.go
 
 #Linux版的binary
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build/linux/prerke_linux-amd64 cmd/main.go 
